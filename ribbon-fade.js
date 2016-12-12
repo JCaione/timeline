8eb9c681f3ea1936f7fa5a8cc58018d8a112b3f5
JCaione <jcaione@cavs.com>
2016-12-12 10:30:53 -0500

    Add files via upload

---
+++ ribbon-fade.js
@@ -0,0 +1,216 @@
+// JavaScript Document
+
+if(screen.width < 1024) { 
+    // do any 480 width stuff here, or simply do nothing
+
+} else {
+    // do all your cool stuff here for larger screens
+
+$(document).ready(function() {
+	function isScrolledIntoView(elem) {
+	    var docViewTop = $(window).scrollTop();
+	    var docViewBottom = docViewTop + $(window).height();
+	
+	    var elemTop = $(elem).offset().top;
+	    var elemBottom = elemTop + $(elem).height();
+	
+	    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom));
+		}  
+		
+		var myelement1 = $('#indicator1');
+		var myevent1 = $('#ribbon1');
+		$(window).scroll(function() {
+		    if(isScrolledIntoView(myelement1)) {
+				myevent1.fadeIn(900); 
+		    }
+		     else {
+				myevent1.fadeOut(900);
+		    }
+			
+		var myelement2 = $('#indicator2');
+		var myevent2 = $('#ribbon2');
+		    if(isScrolledIntoView(myelement2)) {
+				myevent2.fadeIn(900); 
+		    }
+		     else {
+				myevent2.fadeOut(900);
+		    }
+			
+		var myelement3 = $('#indicator3');
+		var myevent3 = $('#ribbon3');
+		    if(isScrolledIntoView(myelement3)) {
+				myevent3.fadeIn(900); 
+		    }
+		     else {
+				myevent3.fadeOut(900);
+		    }			
+			
+		var myelement4 = $('#indicator4');
+		var myevent4 = $('#ribbon4');
+		    if(isScrolledIntoView(myelement4)) {
+				myevent4.fadeIn(900); 
+		    }
+		     else {
+				myevent4.fadeOut(900);
+		    }
+			
+		var myelement5 = $('#indicator5');
+		var myevent5 = $('#ribbon5');
+		    if(isScrolledIntoView(myelement5)) {
+				myevent5.fadeIn(900); 
+		    }
+		     else {
+				myevent5.fadeOut(900);
+		    }
+			
+		var myelement6 = $('#indicator6');
+		var myevent6 = $('#ribbon6');
+		    if(isScrolledIntoView(myelement6)) {
+				myevent6.fadeIn(900); 
+		    }
+		     else {
+				myevent6.fadeOut(900);
+		    }
+			
+		var myelement7 = $('#indicator7');
+		var myevent7 = $('#ribbon7');
+		    if(isScrolledIntoView(myelement7)) {
+				myevent7.fadeIn(900); 
+		    }
+		     else {
+				myevent7.fadeOut(900);
+		    }
+			
+					var myelement8 = $('#indicator8');
+		var myevent8 = $('#ribbon8');
+		    if(isScrolledIntoView(myelement8)) {
+				myevent8.fadeIn(900); 
+		    }
+		     else {
+				myevent8.fadeOut(900);
+		    }
+			
+					var myelement9 = $('#indicator9');
+		var myevent9 = $('#ribbon9');
+		    if(isScrolledIntoView(myelement9)) {
+				myevent9.fadeIn(900); 
+		    }
+		     else {
+				myevent9.fadeOut(900);
+		    }
+			
+					var myelement10 = $('#indicator10');
+		var myevent10 = $('#ribbon10');
+		    if(isScrolledIntoView(myelement10)) {
+				myevent10.fadeIn(900); 
+		    }
+		     else {
+				myevent10.fadeOut(900);
+		    }
+			
+					var myelement11 = $('#indicator11');
+		var myevent11 = $('#ribbon11');
+		    if(isScrolledIntoView(myelement11)) {
+				myevent11.fadeIn(900); 
+		    }
+		     else {
+				myevent11.fadeOut(900);
+		    }
+			
+					var myelement12 = $('#indicator12');
+		var myevent12 = $('#ribbon12');
+		    if(isScrolledIntoView(myelement12)) {
+				myevent12.fadeIn(900); 
+		    }
+		     else {
+				myevent12.fadeOut(900);
+		    }
+			
+					var myelement13 = $('#indicator13');
+		var myevent13 = $('#ribbon13');
+		    if(isScrolledIntoView(myelement13)) {
+				myevent3.fadeIn(900); 
+		    }
+		     else {
+				myevent3.fadeOut(900);
+		    }
+			
+					var myelement14 = $('#indicator14');
+		var myevent14 = $('#ribbon14');
+		    if(isScrolledIntoView(myelement14)) {
+				myevent14.fadeIn(900); 
+		    }
+		     else {
+				myevent14.fadeOut(900);
+		    }	
+			
+					var myelement15 = $('#indicator15');
+		var myevent15 = $('#ribbon15');
+		    if(isScrolledIntoView(myelement15)) {
+				myevent15.fadeIn(900); 
+		    }
+		     else {
+				myevent15.fadeOut(900);
+		    }
+			
+					var myelement16 = $('#indicator16');
+		var myevent16 = $('#ribbon16');
+		    if(isScrolledIntoView(myelement16)) {
+				myevent16.fadeIn(900); 
+		    }
+		     else {
+				myevent16.fadeOut(900);
+		    }			
+			
+					var myelement17 = $('#indicator17');
+		var myevent17 = $('#ribbon17');
+		    if(isScrolledIntoView(myelement17)) {
+				myevent17.fadeIn(900); 
+		    }
+		     else {
+				myevent17.fadeOut(900);
+		    }			
+			
+					var myelement18 = $('#indicator18');
+		var myevent18 = $('#ribbon18');
+		    if(isScrolledIntoView(myelement18)) {
+				myevent18.fadeIn(900); 
+		    }
+		     else {
+				myevent18.fadeOut(900);
+		    }			
+			
+					var myelement19 = $('#indicator19');
+		var myevent19 = $('#ribbon19');
+		    if(isScrolledIntoView(myelement19)) {
+				myevent19.fadeIn(900); 
+		    }
+		     else {
+				myevent19.fadeOut(900);
+		    }			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+					
+			
+									
+			});
+		});		
+		}
+		
